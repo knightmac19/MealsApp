2e6f87003185a5d78a0eca66065bf77ee0c2dfa5
Patrick Dunn <pmdunn78@gmail.com>
2020-08-13 21:22:43 -0400

    passing params for header config

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -3,13 +3,22 @@ import {
   View, 
   Text, 
   StyleSheet,
-  Button 
+  Button, 
+  Platform 
 } from 'react-native';
 
+import Colors from '../constants/Colors';
+import { CATEGORIES } from '../data/dummy-data';
+
 const CategoryMealsScreen = props => {
+
+  const catId = props.navigation.getParam('categoryId');
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
   return (
     <View style={styles.screen}>
       <Text>You're on the Category Meals Screen!</Text> 
+      <Text>{selectedCategory.title}</Text>
       <Button 
         title="See Meal Details"
         onPress={() => props.navigation.navigate('MealDetail')}
@@ -21,6 +30,22 @@ const CategoryMealsScreen = props => {
     </View>
   );
 };
+
+CategoryMealsScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam('categoryId');
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+  return ( 
+    {
+      headerTitle: selectedCategory.title,
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : 'white'
+      },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+    }
+  );
+  
+}
 
 const styles = StyleSheet.create({
   screen: {
