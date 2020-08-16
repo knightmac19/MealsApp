73c2b42ad45218c4a6fed9c45c49975e3c63a20a
Patrick Dunn <pmdunn78@gmail.com>
2020-08-16 17:10:52 -0400

    rendering meals list

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -3,19 +3,45 @@ import {
   View, 
   Text, 
   StyleSheet,
-  Button
+  Button,
+  FlatList
 } from 'react-native';
 
-import { CATEGORIES } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealsScreen = props => {
+  const renderMealItem = itemData => {
+    return (
+      <MealItem 
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        onSelectMeal={() => {}}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+      />
+    );
+  };
 
   const catId = props.navigation.getParam('categoryId');
 
   const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+  const displayedMeals = MEALS.filter(
+    meal => meal.categoryIds.indexOf(catId) >= 0
+  );
+  
   return (
     <View style={styles.screen}>
-      <Text>You're on the Category Meals Screen!</Text> 
+      <FlatList 
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={{width: '95%'}}
+      />
+      
+      {/* <Text>You're on the Category Meals Screen!</Text> 
       <Text>{selectedCategory.title}</Text>
       <Button 
         title="See Meal Details"
@@ -24,7 +50,7 @@ const CategoryMealsScreen = props => {
       <Button 
         title="Go Back"
         onPress={() => props.navigation.pop()}
-      />
+      /> */}
     </View>
   );
 };
